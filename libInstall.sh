4b4a750609b5bd3a16b2b60f8dea25270f750a80
MillanK0817 <51349692+MillanK0817@users.noreply.github.com>
2021-04-28 17:19:36 +0800

    Update the Installation of ncurses

--- libInstall.sh
+++ libInstall.sh
@@ -6,7 +6,6 @@ mkdir -p lib
 mkdir -p bin
 unzip ncurses
 cd ncurses
-./configure --prefix $WORKING/lib --enable-widec --with-pthread &> trash.txt
-make -j &> trash.txt
-make -j install &> trash.txt
-rm trash.txt
+./configure --prefix $WORKING/lib --enable-widec --with-pthread
+make -j
+make -j install
